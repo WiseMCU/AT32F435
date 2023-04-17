46a33a6c68ce8c790374e0d7133195bdf56f7b53
ouyang <3162173168@qq.com>
2023-04-17 16:00:20 +0800

    feat:添加AT32F435的RT-Thread nano最小工程

---
+++ README.md
@@ -0,0 +1,12 @@
+### AT32F435的RT-Thread nano最小工程
+
+- 包含RT-Thread nano内核
+
+- 包含Finsh
+
+- 包含串口打印
+
+- 包含RTT打印
+
+  [WiseMCU/AT32F435 at AT32F435 (github.com)](https://github.com/WiseMCU/AT32F435/tree/AT32F435_RT_NANO)
+
---
+++ drivers/board.h
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2006-2023, RT-Thread Development Team
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ *
+ * Change Logs:
+ * Date           Author       Notes
+ * 2023-04-17     RealThread   first version
+ */
+
+#ifndef __BOARD_H__
+#define __BOARD_H__
+
+#ifdef __cplusplus
+extern "C"
+{
+#endif
+
+/*-------------------------- CHIP CONFIG BEGIN --------------------------*/
+
+#define CHIP_FAMILY_AT32
+#define CHIP_SERIES_AT32F4
+#define CHIP_NAME_AT32F435RGT7
+
+/*-------------------------- CHIP CONFIG END --------------------------*/
+
+/*-------------------------- ROM/RAM CONFIG BEGIN --------------------------*/
+
+#define ROM_START              ((uint32_t)0x08000000)
+#define ROM_SIZE               (1024 * 1024)
+#define ROM_END                ((uint32_t)(ROM_START + ROM_SIZE))
+
+#define RAM_START              (0x20000000)
+#define RAM_SIZE               (384 * 1024)
+#define RAM_END                (RAM_START + RAM_SIZE)
+
+/*-------------------------- ROM/RAM CONFIG END --------------------------*/
+
+/*-------------------------- CLOCK CONFIG BEGIN --------------------------*/
+
+#define BSP_CLOCK_SOURCE                  ("HSI")
+#define BSP_CLOCK_SOURCE_FREQ_MHZ         ((int32_t)0)
+#define BSP_CLOCK_SYSTEM_FREQ_MHZ         ((int32_t)180)
+
+/*-------------------------- CLOCK CONFIG END --------------------------*/
+
+/*-------------------------- UART CONFIG BEGIN --------------------------*/
+
+/** After configuring corresponding UART or UART DMA, you can use it.
+ *
+ * STEP 1, define macro define related to the serial port opening based on the serial port number
+ *                 such as     #define BSP_USING_UART1
+ *
+ * STEP 2, according to the corresponding pin of serial port, define the related serial port information macro
+ *                 such as     #define BSP_UART1_TX_PIN       "PA9"
+ *                             #define BSP_UART1_RX_PIN       "PA10"
+ *
+ */
+
+#define BSP_USING_UART1
+#define BSP_UART1_TX_PIN       "PA9"
+#define BSP_UART1_RX_PIN       "PA10"
+
+/*-------------------------- UART CONFIG END --------------------------*/
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __BOARD_H__ */
